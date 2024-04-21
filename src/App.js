603419e2c1ba454e4545c88f788a5d308d35bb9f
Rahul Agarwal <rahul123agarwal99@gmail.com>
2024-04-21 10:30:57 +0530

    TodoAddDelete Component Created & moved code

--- src/App.js
+++ src/App.js
@@ -1,64 +1,10 @@
 import "./App.css";
-import { useState } from "react";
+import TodoAddDelete from "./TodoAddDelete";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-  const [deletedTodos, setDeltedTodos] = useState([]);
-
-  const handleAdd = () => {
-    setTodos([...todos, todo]);
-    setTodo("");
-  };
-
-  const handleDelete = (index) => {
-    const updatedList = todos.filter((ele, i) => {
-      return i != index;
-    });
-    setTodos(updatedList);
-
-    const deletedList = todos.filter((ele, i) => {
-      return i == index;
-    });
-    setDeltedTodos([...deletedTodos, deletedList]);
-  };
-
   return (
     <div className="App">
-      <h1>Todo List</h1>
-      <div>
-        <input
-          type="text"
-          value={todo}
-          onChange={(e) => setTodo(e.target.value)}
-        />
-        <button onClick={handleAdd}>Add Task</button>
-      </div>
-      <div className="container">
-        <h1>Your Tasks</h1>
-        <ul>
-          {todos.map((todo, index) => {
-            return (
-              <div key={index} className="list-container">
-                <li>{todo}</li>
-                <button onClick={() => handleDelete(index)}>Remove</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="container">
-        <h1>Completed Tasks</h1>
-        <ul>
-          {deletedTodos.map((todo, index) => {
-            return (
-              <div key={index} className="list-container">
-                <li className="list-striked">{todo}</li>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+      <TodoAddDelete />
     </div>
   );
 }
